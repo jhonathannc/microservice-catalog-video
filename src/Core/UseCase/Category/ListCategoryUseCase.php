14194b26536b4db31748f8c040f1358fda7527a1
jhonathannc <jhonathannc@gmail.com>
2024-01-04 11:50:41 -0300

    feat: created ListCategoryUseCase

---
+++ src/Core/UseCase/Category/ListCategoryUseCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\UseCase\Category;
+
+use Core\Domain\Repository\ICategoryRepository;
+use Core\UseCase\Category\DTO\CategoryInputDTO;
+use Core\UseCase\Category\DTO\CategoryOutputDTO;
+
+class ListCategoryUseCase
+{
+  public function __construct(
+    protected ICategoryRepository $repository
+  ) {
+  }
+
+  public function execute(CategoryInputDTO $input): CategoryOutputDTO
+  {
+    $category = $this->repository->findById($input->id);
+
+    return new CategoryOutputDTO(
+      id: $category->id(),
+      name: $category->name,
+      description: $category->description,
+      isActive: $category->isActive
+    );
+  }
+}
