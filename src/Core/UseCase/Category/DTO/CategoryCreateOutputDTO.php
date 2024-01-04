8917d9681fbb6a6907710a61074ff48276c5c5c6
jhonathannc <jhonathannc@gmail.com>
2024-01-03 22:15:53 -0300

    feat: added attribute id to CategoryCreateOutputDTO

--- src/Core/UseCase/Category/DTO/CategoryCreateOutputDTO.php
+++ src/Core/UseCase/Category/DTO/CategoryCreateOutputDTO.php
@@ -5,6 +5,7 @@ namespace Core\UseCase\Category\DTO;
 class CategoryCreateOutputDTO
 {
   public function __construct(
+    public string $id,
     public string $name,
     public string $description = '',
     public bool $isActive = true
