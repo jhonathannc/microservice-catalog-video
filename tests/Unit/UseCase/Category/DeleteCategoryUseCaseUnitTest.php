548ca23de147451bfe39fbfcb7c5917a43717b6c
jhonathannc <jhonathannc@gmail.com>
2024-01-04 17:17:51 -0300

    test: created test for delete category entity

---
+++ tests/Unit/UseCase/Category/DeleteCategoryUseCaseUnitTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\UseCase\Category;
+
+use Core\Domain\Entity\Category;
+use Core\Domain\Repository\ICategoryRepository;
+use Core\UseCase\Category\DeleteCategoryUseCase;
+use Core\UseCase\Category\DTO\CategoryInputDTO;
+use Core\UseCase\Category\DTO\DeleteCategory\DeleteCategoryOutputDTO;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use stdClass;
+
+class DeleteCategoryUseCaseUnitTest extends TestCase
+{
+  public function test_delete(): void
+  {
+    $id = (string) Uuid::uuid4()->toString();
+
+    $mockRepo = Mockery::mock(stdClass::class, ICategoryRepository::class);
+    $mockRepo->shouldReceive('delete')->andReturn(true);
+
+    $mockInputDto = Mockery::mock(CategoryInputDTO::class, [$id]);
+
+    $useCase = new DeleteCategoryUseCase($mockRepo);
+    $response = $useCase->execute($mockInputDto);
+
+    $this->assertInstanceOf(DeleteCategoryOutputDTO::class, $response);
+    $this->assertTrue($response->success);
+  }
+
+  public function tearDown(): void
+  {
+    Mockery::close();
+
+    parent::tearDown();
+  }
+}
