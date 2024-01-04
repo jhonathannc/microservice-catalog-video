1fb45ecbf146a087aa1ac43d1cec1262fe91583a
jhonathannc <jhonathannc@gmail.com>
2024-01-04 11:50:20 -0300

    feat: created DTO Input and Output for Category entity

---
+++ src/Core/UseCase/Category/DTO/CategoryInputDTO.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Core\UseCase\Category\DTO;
+
+class CategoryInputDTO
+{
+  public function __construct(
+    public string $id = '',
+  ) {
+  }
+}
---
+++ src/Core/UseCase/Category/DTO/CategoryOutputDTO.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Core\UseCase\Category\DTO;
+
+class CategoryOutputDTO
+{
+  public function __construct(
+    public string $id,
+    public string $name,
+    public string $description = '',
+    public bool $isActive = true
+  ) {
+  }
+}
