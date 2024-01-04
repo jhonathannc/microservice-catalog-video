6e58e989d7dd9133379c518f04ced3595cd2440d
jhonathannc <jhonathannc@gmail.com>
2024-01-03 22:24:08 -0300

    feat: used DTOs for CreateCategoryUseCase

--- src/Core/UseCase/Category/CreateCategoryUseCase.php
+++ src/Core/UseCase/Category/CreateCategoryUseCase.php
@@ -5,6 +5,7 @@ namespace Core\UseCase\Category;
 use Core\Domain\Entity\Category;
 use Core\Domain\Repository\ICategoryRepository;
 use Core\UseCase\Category\DTO\CategoryCreateInputDTO;
+use Core\UseCase\Category\DTO\CategoryCreateOutputDTO;
 
 class CreateCategoryUseCase
 {
@@ -18,9 +19,18 @@ class CreateCategoryUseCase
   public function execute(CategoryCreateInputDTO $input)
   {
     $category = new Category(
-      name: 'FooBar'
+      name: $input->name,
+      description: $input->description,
+      isActive: $input->isActive
     );
 
-    $this->repository->insert($category);
+    $newCategory = $this->repository->insert($category);
+
+    return new CategoryCreateOutputDTO(
+      id: $newCategory->id(),
+      name: $newCategory->name,
+      description: $newCategory->description,
+      isActive: $newCategory->isActive,
+    );
   }
 }
